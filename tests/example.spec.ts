22aa98cbb8658bd7ffec94fdea6a6716dc51885c
Alan Barbosa <alanzinhob@hotmail.com>
2025-07-13 15:29:33 -0300

    Update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,81 +1,88 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('Tela de Login', () => {
-  const baseUrl = process.env.BASE_URL || 'http://localhost:3001/';
+test.describe("Tela de Login", () => {
+  const baseUrl = process.env.BASE_URL || "http://localhost:3001/";
 
-  test('Deve carregar a página de login com título', async ({ page }) => {
+  test("Deve carregar a página de login com título", async ({ page }) => {
     await page.goto(baseUrl);
-    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+    await expect(page.getByRole("heading", { name: "Login" })).toBeVisible();
   });
 
-  test('Deve mostrar mensagens de erro se tentar submeter vazio', async ({ page }) => {
+  test("Deve mostrar mensagens de erro se tentar submeter vazio", async ({
+    page,
+  }) => {
     await page.goto(baseUrl);
-    await page.getByRole('button', { name: 'Entrar' }).click();
+    await page.getByRole("button", { name: "Entrar" }).click();
 
-    await expect(page.getByText('Email is required.')).toBeVisible();
-    await expect(page.getByText('Password is required.')).toBeVisible();
+    await expect(page.getByText("Email is required.")).toBeVisible();
+    await expect(page.getByText("Password is required.")).toBeVisible();
   });
 
-  test('Deve exibir erro com email/senha inválidos', async ({ page }) => {
+  test("Deve exibir erro com email/senha inválidos", async ({ page }) => {
     await page.goto(baseUrl);
 
-    await page.getByLabel('Email').fill('email@invalido.com');
-		await page.locator('input[type="password"]').fill('123');
-    await page.getByRole('button', { name: 'Entrar' }).click();
-    await expect(page.getByText('Email ou Senha Inválidos')).toBeVisible();
+    await page.getByLabel("Email").fill("email@invalido.com");
+    await page.locator('input[type="password"]').fill("123");
+    await page.getByRole("button", { name: "Entrar" }).click();
+    await expect(page.getByText("Email ou Senha Inválidos")).toBeVisible();
   });
 
-  test('Deve mostrar loader ao enviar formulário', async ({ page }) => {
+  test("Deve mostrar loader ao enviar formulário", async ({ page }) => {
     await page.goto(baseUrl);
 
-    await page.getByLabel('Email').fill('teste@email.com');
-		await page.locator('input[type="password"]').fill('123');
+    await page.getByLabel("Email").fill("teste@email.com");
+    await page.locator('input[type="password"]').fill("123");
 
     // Mock da resposta do next-auth (evita login real)
-    await page.route('**/api/auth/callback/credentials**', async (route) => {
+    await page.route("**/api/auth/callback/credentials**", async (route) => {
       await route.fulfill({
         status: 200,
         body: JSON.stringify({ ok: true }),
       });
     });
-    await page.getByRole('button', { name: 'Entrar' }).click();
-    await expect(page.getByLabel('Carregando Spinner')).toBeVisible();
+    await page.getByRole("button", { name: "Entrar" }).click();
+    await expect(page.getByLabel("Carregando Spinner")).toBeVisible();
   });
 
-	test('Deve entrar na tela de testes USUARIO', async ({ page }) => {
+  test("Deve entrar na tela de testes USUARIO", async ({ page }) => {
     await page.goto(baseUrl);
-		await page.getByLabel('Email').fill('teste@teste.com');
-		await page.locator('input[type="password"]').fill('123');
-    await page.getByRole('button', { name: 'Entrar' }).click();
+    await page.getByLabel("Email").fill("teste@teste.com");
+    await page.locator('input[type="password"]').fill("123");
+    await page.getByRole("button", { name: "Entrar" }).click();
 
-		await page.waitForURL('**/testandouser');
-		await expect(page.getByText('Olá , bem vindo !')).toBeVisible();
-		await expect(page.getByText('Você está na página de Testes e você é um Usuário')).toBeVisible();
-
+    await page.waitForURL("**/testandouser");
+    await expect(page.getByText("Olá , bem vindo !")).toBeVisible();
+    await expect(
+      page.getByText("Você está na página de Testes e você é um Usuário")
+    ).toBeVisible();
   });
 
-	test('Deve entrar na tela de testes ADMINISTRADOR', async ({ page }) => {
+  test("Deve entrar na tela de testes ADMINISTRADOR", async ({ page }) => {
     await page.goto(baseUrl);
-		await page.getByLabel('Email').fill('teste1@teste.com');
-		await page.locator('input[type="password"]').fill('123');
-    await page.getByRole('button', { name: 'Entrar' }).click();
+    await page.getByLabel("Email").fill("teste1@teste.com");
+    await page.locator('input[type="password"]').fill("123");
+    await page.getByRole("button", { name: "Entrar" }).click();
 
-		await page.waitForURL('**/admin');
-		await expect(page.getByText('Olá , bem vindo !')).toBeVisible();
-		await expect(page.getByText('Você é um Administrador, bem vindo !')).toBeVisible();
-
+    await page.waitForURL("**/admin");
+    await expect(page.getByText("Olá , bem vindo !")).toBeVisible();
+    await expect(
+      page.getByText("Você é um Administrador, bem vindo !")
+    ).toBeVisible();
   });
 
-	test('Deve deslogar do sistema e voltar na tela de login', async ({ page }) => {
+  test("Deve deslogar do sistema e voltar na tela de login", async ({
+    page,
+  }) => {
     await page.goto(baseUrl);
-		await page.getByLabel('Email').fill('teste@teste.com');
-		await page.locator('input[type="password"]').fill('123');
-    await page.getByRole('button', { name: 'Entrar' }).click();
-		await page.waitForURL('**/testandouser');
-		await expect(page.getByText('Olá , bem vindo !')).toBeVisible();
-		await page.getByRole('button', { name: 'Sair' }).click();
-		await page.waitForURL('**/');
-		await expect(page.getByText('Login')).toBeVisible();
-
+    await page.getByLabel("Email").fill("teste@teste.com");
+    await page.locator('input[type="password"]').fill("123");
+    await page.getByRole("button", { name: "Entrar" }).click();
+    await page.waitForURL("**/testandouser");
+    await expect(page.getByText("Olá , bem vindo !")).toBeVisible();
+    await Promise.all([
+      page.waitForURL("**/"),
+      page.getByRole("button", { name: "Sair" }).click(),
+    ]);
+    await expect(page.getByText("Login")).toBeVisible();
   });
 });
